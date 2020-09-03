6b821bea7836dce71edd4f1bd357facabec0dd9a
Alexander Granhof <alexander.g737@gmail.com>
2020-09-03 16:32:28 +0200

    created constants file for rresponse codes

---
+++ server/src/errors/constants/httpResponseCodes.ts
@@ -0,0 +1,13 @@
+export interface HttpResponseCodeTypes {
+    BAD_REQUEST: 400
+    NOT_FOUND: 404
+    INTERNAL_SERVER_ERROR: 500
+}
+
+const HttpResponseCodes: HttpResponseCodeTypes = {
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+}
+
+export default HttpResponseCodes
