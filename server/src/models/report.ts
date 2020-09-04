9a9b823768c49f369bc21f4fb5e39371e9892a13
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 11:55:31 +0200

    added joi schemas

--- server/src/models/report.ts
+++ server/src/models/report.ts
@@ -1,6 +1,20 @@
-export default interface Report {
-    id: number
+import joi from 'joi'
+
+export interface IncomingReportModel {
     title: string
     content: string
+}
+
+export interface ReportModel extends IncomingReportModel {
     user_id: number
 }
+
+export default interface CreatedReportModel extends ReportModel {
+    id: number
+}
+
+export const ReportSchema = joi.object({
+    title: joi.string().required(),
+    content: joi.string().required(),
+    user_id: joi.number().required(),
+})
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,5 +1,15 @@
-export default interface User {
-    id: number
+import joi from 'joi'
+
+export interface UserModel {
     username: string
     password: string
 }
+
+export interface CreatedUserModel extends UserModel {
+    id: number
+}
+
+export const UserSchema = joi.object({
+    username: joi.string().required(),
+    password: joi.string().required(),
+})
