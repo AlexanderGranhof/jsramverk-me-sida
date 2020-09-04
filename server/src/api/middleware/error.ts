35ad3c486317c9606da779357172d67ccb7bddd6
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 15:12:50 +0200

    added a sqlite error handler

--- server/src/api/middleware/error.ts
+++ server/src/api/middleware/error.ts
@@ -10,7 +10,7 @@ export const HttpErrorHandler = (error: unknown, req: Request, res: Response, ne
         return next(error)
     }
 
-    return res.status(error.statusCode).send(error.data)
+    return res.status(error.statusCode).json({ message: error.message, data: error.data })
 }
 
 export const JoiErrorHandler = (error: unknown, req: Request, res: Response, next: NextFunction) => {
@@ -19,4 +19,21 @@ export const JoiErrorHandler = (error: unknown, req: Request, res: Response, nex
     }
 
     return res.status(BAD_REQUEST).send(error.message)
+}
+
+// This is temporary for sqlite 'UNIQUE' errors
+export const SqliteErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+    if (!error?.code?.startsWith('SQLITE')) {
+        return next(error)
+    }
+
+    const SqliteErrorCode = error.code
+
+    const MATCH_TABLE_COLUMN = new RegExp(`${SqliteErrorCode}:.*:[^(.*?)\.]*\.(.*)`)
+    const MATCH_SQLITE_ERROR = new RegExp(`${SqliteErrorCode}: *(.*):`)
+
+    const column = error.message.match(MATCH_TABLE_COLUMN)[1]
+    const message = error.message.match(MATCH_SQLITE_ERROR)[1]
+
+    return res.status(BAD_REQUEST).send(`${message} for '${column}'`)
 }
