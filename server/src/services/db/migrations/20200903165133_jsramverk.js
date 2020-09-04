705a9cdee9dace40a1ef2c94d39f9c088f3571af
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 12:28:52 +0200

    username should be unique

--- server/src/services/db/migrations/20200903165133_jsramverk.js
+++ server/src/services/db/migrations/20200903165133_jsramverk.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
     const users = knex.schema.createTable('users', (table) => {
         table.increments('id')
-        table.string('username')
+        table.string('username').unique()
         table.string('password')
     })
 
