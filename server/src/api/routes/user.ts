4f00e98e42f12cee3f9998c4da471627725f4220
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 15:12:29 +0200

    created a user route

---
+++ server/src/api/routes/user.ts
@@ -0,0 +1,50 @@
+import { Router } from 'express'
+import { wrapAsync } from '../middleware/async'
+import { UserSchema } from '../../models/user'
+import * as User from '../../services/user'
+
+const router = Router()
+
+router.post(
+    '/register',
+    wrapAsync(async (req, res) => {
+        const user = await UserSchema.validateAsync(req.body)
+
+        await User.create(user)
+
+        res.sendStatus(201)
+    }),
+)
+
+router.post(
+    '/login',
+    wrapAsync(async (req, res) => {
+        console.log(req.session)
+        const user = await UserSchema.validateAsync(req.body)
+        const userDBObject = await User.validate(user)
+
+        if (!userDBObject) {
+            return res.sendStatus(401)
+        }
+
+        if (!req.session) {
+            return res.status(500).send('undefined session')
+        }
+
+        req.session.user = userDBObject
+
+        return res.sendStatus(200)
+    }),
+)
+
+router.post('/logout', (req, res) => {
+    if (!req.session) {
+        return res.status(500).send('undefined session')
+    }
+
+    req.session.destroy((err) => {
+        return res.status(err ? 500 : 200).send(err ? err : 'ok')
+    })
+})
+
+export default router
