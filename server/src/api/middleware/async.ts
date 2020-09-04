aadbfba2257704ddca37d4083eb92d807736888c
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 11:54:38 +0200

    created a middleware wrapper to handle async errors

---
+++ server/src/api/middleware/async.ts
@@ -0,0 +1,11 @@
+import { Response, NextFunction, Request } from 'express'
+
+type MiddlewareFunc = (req: Request, res: Response, next: NextFunction) => void
+
+export const wrapAsync = (fn: MiddlewareFunc) => (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    return Promise.resolve(fn(req, res, next)).catch(next)
+}
