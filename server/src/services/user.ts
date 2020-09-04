ee72a993c2461c7a6f33dc74aeb8557f8d0910d1
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 15:11:45 +0200

    added more methods to the user service to validate and create

--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -1,7 +1,29 @@
 import db from './db'
+import { UserModel, CreatedUserModel } from '../models/user'
+import * as bcrypt from './bcrypt'
 
-const users = db('users')
+const users = () => db('users')
 
 export const all = () => {
-    return users.select('*')
+    return users().select('*')
+}
+
+export const create = async (user: UserModel) => {
+    const password = await bcrypt.hash(user.password)
+
+    return users().insert({ ...user, password })
+}
+
+export const validate = async (user: UserModel) => {
+    const { username, password } = user
+
+    const foundUser: CreatedUserModel | undefined = await users().where({ username }).first()
+
+    if (!foundUser) {
+        return false
+    }
+
+    const valid = bcrypt.compare(password, foundUser.password)
+
+    return valid ? foundUser : false
 }
