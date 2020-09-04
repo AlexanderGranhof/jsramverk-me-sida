d7a8ad7c2b2afd28cea76c37c3e1edae913d6686
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 12:28:16 +0200

    added a error handler for joi errors

--- server/src/api/middleware/error.ts
+++ server/src/api/middleware/error.ts
@@ -1,10 +1,22 @@
 import { Request, Response, NextFunction } from 'express'
 import { HttpError } from '../../errors/http'
+import HttpResponseCodes from '../../errors/constants/httpResponseCodes'
+import { ValidationError } from 'joi'
 
-export default (error: unknown, req: Request, res: Response, next: NextFunction) => {
+const { BAD_REQUEST } = HttpResponseCodes
+
+export const HttpErrorHandler = (error: unknown, req: Request, res: Response, next: NextFunction) => {
     if (!(error instanceof HttpError)) {
         return next(error)
     }
 
     return res.status(error.statusCode).send(error.data)
+}
+
+export const JoiErrorHandler = (error: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (!(error instanceof ValidationError)) {
+        return next(error)
+    }
+
+    return res.status(BAD_REQUEST).send(error.message)
 }
