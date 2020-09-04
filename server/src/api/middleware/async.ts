d71ad23723b00ef7e7f2af0db08c001ca2cedeae
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 11:56:48 +0200

    next should be optional

--- server/src/api/middleware/async.ts
+++ server/src/api/middleware/async.ts
@@ -1,6 +1,6 @@
 import { Response, NextFunction, Request } from 'express'
 
-type MiddlewareFunc = (req: Request, res: Response, next: NextFunction) => void
+type MiddlewareFunc = (req: Request, res: Response, next?: NextFunction) => void
 
 export const wrapAsync = (fn: MiddlewareFunc) => (
     req: Request,
