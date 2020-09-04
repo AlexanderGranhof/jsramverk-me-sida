59e8ed6fbd9ea368f88d507a4a10cd4ac32ba378
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 12:28:45 +0200

    added new handlers to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,20 +3,31 @@ import express from 'express'
 import chalk from 'chalk'
 import morgan from 'morgan'
 
-import ErrorHandler from './api/middleware/error'
-import RootRoute from './api/routes/root'
+import { HttpErrorHandler, JoiErrorHandler } from './api/middleware/error'
 
-// Load env variables from .env file
-dotenv.config()
+import RootRoute from './api/routes/root'
+import UserRoute from './api/routes/user'
+
+dotenv.config() // Load env variables from .env file
 
 const app = express()
 const port = process.env.PORT || 3000
 const { NODE_ENV } = process.env
 
-app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'))
-app.use(RootRoute)
+/* Request pre-processor middleware */
 
-app.use(ErrorHandler)
+app.use(express.json())
+app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'))
+
+/* API routes */
+
+app.use(RootRoute)
+app.use(UserRoute)
+
+/* Error handler middleware */
+
+app.use(HttpErrorHandler)
+app.use(JoiErrorHandler)
 
 app.listen(port, () => {
     console.log(chalk.green(`Server listening to port ${port}`))
