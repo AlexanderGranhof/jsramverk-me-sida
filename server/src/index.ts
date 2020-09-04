7580d0c819c54d3721c05b62983e12b4818b45fe
Alexander Granhof <alexander.g737@gmail.com>
2020-09-04 15:13:19 +0200

    added express-session, report route and sqlite error handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,14 @@ import dotenv from 'dotenv'
 import express from 'express'
 import chalk from 'chalk'
 import morgan from 'morgan'
+import crypto from 'crypto'
+import session from 'express-session'
 
-import { HttpErrorHandler, JoiErrorHandler } from './api/middleware/error'
+import { HttpErrorHandler, JoiErrorHandler, SqliteErrorHandler } from './api/middleware/error'
 
 import RootRoute from './api/routes/root'
 import UserRoute from './api/routes/user'
+import ReportRoute from './api/routes/report'
 
 dotenv.config() // Load env variables from .env file
 
@@ -14,20 +17,32 @@ const app = express()
 const port = process.env.PORT || 3000
 const { NODE_ENV } = process.env
 
+app.set('trust proxy', 1) // trust nginx
+
 /* Request pre-processor middleware */
 
 app.use(express.json())
 app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'))
+app.use(
+    session({
+        secret: crypto.randomBytes(64).toString('hex'),
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    }),
+)
 
 /* API routes */
 
 app.use(RootRoute)
 app.use(UserRoute)
+app.use(ReportRoute)
 
 /* Error handler middleware */
 
 app.use(HttpErrorHandler)
 app.use(JoiErrorHandler)
+app.use(SqliteErrorHandler)
 
 app.listen(port, () => {
     console.log(chalk.green(`Server listening to port ${port}`))
