b6e0cb3f6fc783a6afe724ebe49afd876e331518
Alexander Granhof <alexander.g737@gmail.com>
2020-09-06 16:31:46 +0200

    only use  the route component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,14 +7,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import Nav from './components/nav/nav'
 
-import About from './reports/about/about'
-import Kmom01 from './reports/kmom01/kmom01'
-import Kmom02 from './reports/kmom02/kmom02'
-import Kmom03 from './reports/kmom03/kmom03'
-import Kmom04 from './reports/kmom04/kmom04'
-import Kmom05 from './reports/kmom05/kmom05'
-import Kmom06 from './reports/kmom06/kmom06'
-import Kmom10 from './reports/kmom10/kmom10'
+import Report from './components/report/report'
 
 const App: FunctionComponent = () => {
     return (
@@ -25,16 +18,17 @@ const App: FunctionComponent = () => {
                     <Route
                         render={({ location }) => (
                             <TransitionGroup>
-                                <CSSTransition key={location.key} timeout={5000} classNames="page">
+                                <CSSTransition
+                                    key={location.pathname}
+                                    timeout={5000}
+                                    classNames="page"
+                                >
                                     <Switch location={location}>
-                                        <Route exact path="/" component={About} />
-                                        <Route path="/reports/week/1" component={Kmom01} />
-                                        <Route path="/reports/week/2" component={Kmom02} />
-                                        <Route path="/reports/week/3" component={Kmom03} />
-                                        <Route path="/reports/week/4" component={Kmom04} />
-                                        <Route path="/reports/week/5" component={Kmom05} />
-                                        <Route path="/reports/week/6" component={Kmom06} />
-                                        <Route path="/reports/week/7" component={Kmom10} />
+                                        <Route exact path="/" component={Report} />
+                                        <Route
+                                            path="/reports/week/:weekNumber"
+                                            component={Report}
+                                        />
                                     </Switch>
                                 </CSSTransition>
                             </TransitionGroup>
