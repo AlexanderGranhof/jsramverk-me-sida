f49a277b61e945064e904e887cca383a3a9989f5
Alexander Granhof <alexander.g737@gmail.com>
2020-09-07 19:00:36 +0200

    fixed bug with JSON, need to add content type when sending json

--- client/src/services/fetch.ts
+++ client/src/services/fetch.ts
@@ -2,10 +2,14 @@ const host = `${window.location.protocol}//${window.location.hostname}:3001`
 
 export default (path = '', body?: Record<string, any>) => {
     const url = path.startsWith('/') ? host + path : `${host}/${path}`
+    const headers = body ? { 'Content-Type': 'application/json' } : undefined
+
+    console.log(body)
 
     return fetch(url, {
         method: body ? 'POST' : 'GET',
         credentials: 'same-origin',
         body: body ? JSON.stringify(body) : undefined,
+        headers,
     })
 }
