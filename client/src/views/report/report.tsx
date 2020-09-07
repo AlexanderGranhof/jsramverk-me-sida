05184fbc8f2442e3959cd1fba5207b96284d5a76
Alexander Granhof <alexander.g737@gmail.com>
2020-09-07 21:00:04 +0200

    created basic report view

---
+++ client/src/views/report/report.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+const Report: FunctionComponent = () => {
+    return (
+        <div>
+            <h1>report</h1>
+        </div>
+    )
+}
+
+export default Report
