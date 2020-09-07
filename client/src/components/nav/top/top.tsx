189a1dbddf76ed1d5fab1424e460275a0b933d0d
Alexander Granhof <alexander.g737@gmail.com>
2020-09-07 21:00:29 +0200

    have two different menus depending on auth context

--- client/src/components/nav/top/top.tsx
+++ client/src/components/nav/top/top.tsx
@@ -1,12 +1,18 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useContext } from 'react'
 import styles from './top.module.scss'
+import { authContext } from '../../../contexts/auth'
 
 type TopNavProps = {
     onClick?: (listItem: string) => void
 }
 
 const Top: FunctionComponent<TopNavProps> = (props) => {
-    const items = ['sign in', 'register']
+    const signedOutMenu = ['register', 'sign in']
+    const signedInMenu = ['edit reports', 'sign out']
+
+    const [auth] = useContext(authContext)
+
+    const items = auth.authenticated ? signedInMenu : signedOutMenu
 
     const handleClick = (item: string) => {
         typeof props.onClick === 'function' && props.onClick(item)
