637bdb6b6496bda88196f56b3b3643724bd73ac6
Alexander Granhof <alexander.g737@gmail.com>
2020-09-08 14:15:04 +0200

    added type option to buttons

--- client/src/components/button/button.tsx
+++ client/src/components/button/button.tsx
@@ -3,13 +3,16 @@ import styles from './button.module.scss'
 
 type ButtonProps = {
     onClick?: () => void
-    type?: 'small' | 'normal' | 'large'
+    size?: 'small' | 'normal' | 'large'
+    type?: 'normal' | 'primary' | 'danger'
     className?: string
 }
 
 const Button: FunctionComponent<ButtonProps> = (props) => {
-    const { children, onClick, type = 'normal', className = '' } = props
-    const buttonClassName = `${className} ${styles['button']} ${styles[type]}`
+    const { children, onClick, size = 'normal', type = 'normal', className = '' } = props
+    const buttonClassName = `${className} ${styles['button']} ${styles[size]} ${
+        styles[`type-${type}`]
+    }`
 
     return (
         <button onClick={onClick?.bind(null)} className={buttonClassName}>
