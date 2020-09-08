9bce594f88449fb655175ab08b4fb8022bde387e
Alexander Granhof <alexander.g737@gmail.com>
2020-09-08 14:14:02 +0200

    added update method

--- server/src/services/report.ts
+++ server/src/services/report.ts
@@ -15,3 +15,9 @@ export const week = (week: string | number) => {
 export const create = (user: CreatedUserModel, report: IncomingReportModel) => {
     return reports().insert({ ...report, user_id: user.id })
 }
+
+export const update = (user: CreatedUserModel, report: IncomingReportModel) => {
+    const { content } = report
+
+    return reports().where({ week: report.week }).update({ content, user_id: user.id })
+}
