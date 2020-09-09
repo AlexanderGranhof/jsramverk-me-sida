7947a25d62db3d847893d72a4f951086210843ac
Alexander Granhof <alexander.g737@gmail.com>
2020-09-09 11:24:54 +0200

    removed log

--- client/src/services/fetch.ts
+++ client/src/services/fetch.ts
@@ -4,8 +4,6 @@ export default (path = '', body?: Record<string, any>) => {
     const url = path.startsWith('/') ? host + path : `${host}/${path}`
     const headers = body ? { 'Content-Type': 'application/json' } : undefined
 
-    console.log(body)
-
     return fetch(url, {
         method: body ? 'POST' : 'GET',
         credentials: 'include',
