ab3c6e22d73a0c0087332532ffdba0b79a5d8b35
Alexander Granhof <alexander.g737@gmail.com>
2020-09-15 14:14:39 +0200

    use a json isntead of yaml so we can import it

--- server/src/swagger/middleware.ts
+++ server/src/swagger/middleware.ts
@@ -1,9 +1,4 @@
-import fs from 'fs'
-import path from 'path'
-import yaml from 'js-yaml'
 import swagger from 'swagger-ui-express'
-
-const yamlSwaggerDoc = fs.readFileSync(path.resolve(__dirname, 'swaggerfile.yml'), 'utf8')
-const jsonSwaggerDoc = yaml.safeLoad(yamlSwaggerDoc) as Record<string, unknown>
+import jsonSwaggerDoc from './swaggerfile.json'
 
 export default [swagger.serve, swagger.setup(jsonSwaggerDoc)]
