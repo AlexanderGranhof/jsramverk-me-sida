541ddca8c410e49770bba0989a2588b0ec2584be
Alexander Granhof <alexander.g737@gmail.com>
2020-09-27 21:09:23 +0200

    updated for .travis.yml

--- test-server.bash
+++ test-server.bash
@@ -1,6 +1,6 @@
 docker-compose -f docker-compose-test.yml up -d server
 
-sleep 5
+sleep 10
 
 docker-compose -f docker-compose-test.yml up test-server
 docker-compose -f docker-compose-test.yml down
