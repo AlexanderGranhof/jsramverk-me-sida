c03f37a662a46525ccc83fb084bd6bab7cd7f05a
Alexander Granhof <alexander.g737@gmail.com>
2020-10-05 00:11:07 +0200

    changed chat service to use mongodb with sqlite as fallback

--- server/src/services/chat.ts
+++ server/src/services/chat.ts
@@ -1,12 +1,23 @@
 import db from './db'
 import { Message } from '../models/chat'
+import mongo from './mongo'
 
 const chat = () => db('chat')
 
-export const all = () => {
-    return chat().select('*')
+export const all = async () => {
+    try {
+        return (await mongo).Message.find()
+    } catch (err) {
+        console.error(err)
+        return chat().select('*')
+    }
 }
 
-export const create = (message: Message) => {
-    return chat().insert(message)
+export const create = async (message: Message) => {
+    try {
+        return new (await mongo).Message({ ...message }).save()
+    } catch (err) {
+        console.error(err)
+        return chat().insert(message)
+    }
 }
